22b8005ce87795157118606b93cdd846b00188ad
Santino <santino.cervera@gmail.com>
2022-09-23 14:45:06 +0200

    MyLinkedList & SearchTree done

--- src/com/santinocc/Main.java
+++ src/com/santinocc/Main.java
@@ -1,5 +1,24 @@
+package com.santinocc;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
+
+        String stringData = "5 7 3 9 8 2 1 0 4 6";
+
+        String[] data = stringData.split(" ");
+        for (String s : data) {
+            tree.addItem(new Node(s));
+        }
+
+        tree.traverse(tree.getRoot());
+
+        tree.removeItem(new Node("3"));
+        tree.traverse((tree.getRoot()));
+
+        tree.removeItem(new Node("5"));
+        tree.traverse((tree.getRoot()));
     }
 }
--- src/com/santinocc/NodeList.java
+++ src/com/santinocc/NodeList.java
@@ -1,2 +1,9 @@
-package com.santinocc;public interface NodeList {
+package com.santinocc;
+
+public interface NodeList {
+
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void traverse(ListItem root);
 }
